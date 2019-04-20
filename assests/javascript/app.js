93bbdd1ccaa3d23bd52825e456587f5fe02a73a8
Stephen Malito <stevemalito@gmail.com>
2019-04-19 19:39:27 -0500

    readme ud

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -28,3 +28,24 @@ $("#addNewTopic").on("click", function(){
 
 
 });
+
+let state = $(this).attr("data-state")
+      let animateGif = $(this).attr("data-animate")
+      let stillGif = $(this).attr("data-still")
+      let still = "still";
+      let animate = "animate";
+
+
+      if (state == "still"){
+        $(this).attr("src", animateGif)
+        $(this).attr("data-state", animate)
+        console.log(state)
+        
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
