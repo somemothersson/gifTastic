e003604b8600786f4586cd928f27bbf94b29be9f
Stephen Malito <stevemalito@gmail.com>
2019-04-19 23:27:29 -0500

    javascript

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,12 +1,6 @@
-//buttons
-    //when a button is clicked
-    //load 10 gipfy stills on the page
-    //load rating for each gipfy with it
-        //each gif will display it's rating
+
 //gif button functions
-    //when a still gif is clicked the image should begin to animate
-        //replace src with "data-animate" value
-        //change "data-status", animate
+    
     //when an animated gif is clicked it should change to be still
         //replace src with "data-still" value
         //change "data-status", still
@@ -17,11 +11,17 @@
         //append searched term to "data-topic" attr
             
             
+//declare varibles
 
+//array to hold basic images
 let topics = ["dog", "dawg", "cats"];
+//variable to store the search term
 let search = "";
+//the number of gifs to return on query, can be adjusted to change results dynamically
 let limit = 10;
 
+
+//function to create buttons for the base items in the array
 function rendButtons (){
     $("#buttons").empty()
     for (let i = 0; i < topics.length; i++){
@@ -33,10 +33,25 @@ function rendButtons (){
     }
    
 }
+//load buttons
 rendButtons()
-//fill gifs
 
-    //jquery to grab "data-topic" set to search value
+//function to add user defined searchs to button array
+$("#addNewTopic").on("click", function(){
+    event.preventDefault();
+    //jQuery variable to grab the input value of the form 
+    let newTopic = $("#newTopic").val().trim()
+    //push to the new topic to the array
+    topics.push(newTopic);
+    //will reload all existing buttons and add newly created as it is now part of the array
+    rendButtons()
+
+
+});
+
+
+
+    
     
     
     //query gify api with "data-attr" value
@@ -46,8 +61,11 @@ $(document).on("click", "img", function() {
     animate = $(this).attr("data-animate")
     status = $(this).attr("data-status")
     console.log("clicked")
-
+//when a still gif is clicked the image should begin to animate
+        
+        //change "data-status", animate
     if (status == "still"){
+        //replace src with "data-animate" value
         $(this).attr("src", animate)
         $(this).attr("data-status", "animate")
         
@@ -65,81 +83,56 @@ $(document).on("click", "img", function() {
 });
 
 
+//When a topic button is clicked - this function holds the API Get and appending of images to DIV
 $(document).on("click", ".topicButton",function(){
+    //empty any existing gifs
     $("#gifs").empty()
-    
-    search = $(this).attr("data-topic")
-    console.log(search);
-    //  jquery to grab "data-topic" set to search value
+    //jquery to grab "data-topic" set to search value
    
+    search = $(this).attr("data-topic")
+   
+    //pass search term, and limit into api key that includes rating preference
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&&rating=pg&&limit=" + limit + "&&api_key=Ss4Q4xQlkM4F26E43IeKPi92k0s4W3oW";
-    //
 
+    //JSON API GET call
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response) {
-        for (let i = 0; i < 11; i++){
-        // console.log(response.data[i]);
+        //loop to create individual items based on the limit
+        for (let i = 0; i < limit+1 ; i++){
+       
 
-         //create variable to capture what information from JSON you want to capture
+         //variables to capture pertinent information from JSON you want to capture still and animated URLS +  rating
          
          let animateUrl = response.data[i].images.fixed_height_small.url;
          let stillUrl = response.data[i].images.fixed_height_small_still.url;
          let rating = response.data[i].rating;
 
-       
+         //jqeury varibles to make creating new elements cleaner and easier to update
+        let figure = $("<imgcaption>")
          let gif = $("<img>");
+         let rater = $("<h3>")
+       
            
 
-         //create image elements with tattributes of the specified source and alt
+         //apply neccesary attributes to each element: source, alt, seperate links for still and animated images 
+         //+ an attr for animate/still control. The still image will be loaded by default and the animate stored to
+         //allow toggleing between still and animated
          gif.attr({"src": stillUrl, "alt": "gif", "data-rating": rating, 
          "data-still": stillUrl, "data-animate": animateUrl, "data-status": "still"});
          
 
-         //prepend each image to the images div, so each image request
+         //append each image/gif to the images div, with Rating
          
-        console.log(rating)
-        console.log(stillUrl)
-        console.log(animateUrl)
-        
-        $("#gifs").prepend(gif); 
+        $("#gifs").append(gif, rater)
+      
         }
         
     });
   
-      //variable to store the jQuery object for repeated use
-         //the following attributs need to be GOT from gify and saved as attributes to the element via jquery
-            //"src", response.fixed_height_small_still.url
-            //"data-rating", response.rating
-            //"data-still", response.fixed_height_small_still.url
-            //"data-animate", response.fixed_height_small.url
-            //"data-status", "still"
-
+   
 
 });
 
-$("#addNewTopic").on("click", function(){
-    event.preventDefault();
-
-    let newTopic = $("#newTopic").val().trim()
-    console.log(newTopic);
-    topics.push(newTopic);
-    rendButtons()
-
-
-});
-
-
-// let state = $(this).attr("data-state")
-//       let animateGif = $(this).attr("data-animate")
-//       let stillGif = $(this).attr("data-still")
-//       let still = "still";
-//       let animate = "animate";
-
-
-//       if (state == "still"){
-//         $(this).attr("src", animateGif)
-//         $(this).attr("data-state", animate)
-//         console.log(state)
